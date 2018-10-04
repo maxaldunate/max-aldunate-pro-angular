2c346550136e832cacc63d29a4c6265e515d3998
Max Aldunate <maxaldunate@gmail.com>
2018-10-04 09:13:29 +0200

    Invalid template path

--- aws/deploy-scripts/deploy.py
+++ aws/deploy-scripts/deploy.py
@@ -15,39 +15,6 @@ def cfgLoad(filePath):
 def pathBuild(cfg, path, file):
 	return cfg['base-dir'] + path + "\\" + file
 
-def runStack(cfg, bucketDeployPackages):
-	for st in cfg['stacks']:
-		stackFullName = cfg['stack-base-name'] + st['name']
-		fullFile = pathBuild(cfg, st['path'], st['template-file'])
-		outputDir = pathBuild(cfg, cfg['output-dir-stacks'], st['template-file'])
-
-		print("=======================================================================")
-		print("Stack Name: " +  stackFullName)
-		print("Stack Path: " +  st['path'])
-		print("Stack File: " +  st['template-file'])
-		
-		print()
-		print("$ package command .............")
-		cmdPackege = "aws cloudformation package --template-file " + fullFile + \
-			" --output-template-file " + fullFile + "-deploy.yaml" + \
-			" --s3-bucket " + bucketDeployPackages + " " \
-			" --profile " + cfg['profile-user']
-		print(cmdPackege)
-		print(subprocess.check_output([cmdPackege]))
-		
-
-		print()
-		print("$ deploy command .............")
-		cmdDeploy = "aws cloudformation deploy " + \
-        		" --region " + cfg['aws-region'] + \
-        		" --template-file " + fullFile + "-deploy.yaml " + \
-        		" --stack-name " + stackFullName + \
-        		" --capabilities CAPABILITY_IAM " + \
-        		" --profile " + cfg['profile-user'] + \
-        		" --tags Project=" + cfg['project-tag'] + \
-        		" --parameter-overrides TagProject=" + cfg['project-tag']
-		print(cmdPackege)
-
 def awsSession(cfg):
 	boto3.setup_default_session(profile_name=cfg['profile-user'])
 	boto3.setup_default_session(region_name=cfg['aws-region'])
@@ -59,42 +26,37 @@ def composeStackDescribeCommand(cfg, fullOutputFile, stackName):
     	" --region " + cfg['aws-region'] + \
     	" > " + fullOutputFile
 
-def runShell(command):
-	print("------------------------------------ starts command -------------------------------")
+def runShell(command, commandName):
+	print("------------------------------------ " + commandName)
 	print(command)
 	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 	process.wait()
 	print(process.returncode)
-	print("------------------------------------ end command ----------------------------------")
 
-def extractOutputs(describeFile, configOutputFile, stackName):
+def extractOutputs(describeFile, cfgOutFile, stackName, simpleStackName):
 	outputDict = {"Outputs": []}
 	with open(describeFile, 'r') as f:
 		describeDict = json.load(f)
-
 	for st in describeDict["Stacks"]:
 		if(st["StackName"]==stackName):
 			for out in st["Outputs"]:
-				out["StackName"] = stackName
+				out["StackName"] = simpleStackName
 				outputDict["Outputs"].append(out)
 
-	with open(configOutputFile, 'r') as f:
+	with open(cfgOutFile, 'r') as f:
 		data = json.load(f)
-
 	for out in outputDict["Outputs"]:
 		data["Outputs"].append(out)
-
-	with open(configOutputFile, 'w') as fw:
+	with open(cfgOutFile, 'w') as fw:
 		json.dump(data, fw, ensure_ascii=False, sort_keys=True, indent=2)
 
+def runShellStack(command, cfg, stackName, simpleStackName, cfgOutFile):
+	runShell(command, simpleStackName)
+	fullOutputFile = pathBuild(cfg, cfg['output-dir-stacks'], stackName + "-output.json")
+	runShell(composeStackDescribeCommand(cfg, fullOutputFile, stackName), simpleStackName + " decribe stack ...")
+	extractOutputs(fullOutputFile, cfgOutFile, stackName, simpleStackName)
 
-def runShellStack(command, cfg, stackName, configOutputFile):
-	runShell(command)
-	fullOutputFile = cfg['base-dir'] + cfg['output-dir-stacks'] + "\\" + stackName + "-output.json"
-	runShell(composeStackDescribeCommand(cfg, fullOutputFile, stackName))
-	extractOutputs(fullOutputFile, configOutputFile, stackName)
-
-def runPrerequisitesStack(cfg, configOutputFile):
+def runPrerequisitesStack(cfg, cfgOutFile):
 	print("===================================================================================")
 	print("Prerequisite Stack")
 	stackName = cfg['stack-base-name'] + "-prerequisites"
@@ -109,26 +71,81 @@ def runPrerequisitesStack(cfg, configOutputFile):
     	" --tags Project=" + cfg['project-tag'] + \
     	" --parameter-overrides TagProject=" + cfg['project-tag'] + \
     	" AppName=" + cfg['stack-base-name']
-	runShellStack(command, cfg, stackName, configOutputFile)
+	runShellStack(command, cfg, stackName, "prerequisites", cfgOutFile)
 	
 def configOutputFileInitialize(cfg):
-	configOutputFile = cfg['base-dir'] + cfg['output-dir-stacks'] + "\\config-output.json"
-	print(configOutputFile)
-	if os.path.isfile(configOutputFile):
-		print("Removing config-output.json file at " + configOutputFile)
-		os.remove(configOutputFile)
-	with open(configOutputFile, 'w') as f:
+	cfgOutFile = pathBuild(cfg, cfg['output-dir-stacks'], "\\config-output.json")
+	print(cfgOutFile)
+	if os.path.isfile(cfgOutFile):
+		print("Removing config-output.json file at " + cfgOutFile)
+		os.remove(cfgOutFile)
+	with open(cfgOutFile, 'w') as f:
 		json.dump({"Outputs": []}, f, ensure_ascii=False, sort_keys=True, indent=2)
 
-	return configOutputFile
+	return cfgOutFile
+
+def getOutputValue(parametersSource, cfgOutFile, nameParameter):
+	oF = {}
+	with open(cfgOutFile, 'r') as f:
+		oF = json.load(f)
+
+	oneParamSource = {}
+	for p in parametersSource:
+		if(p['name']==nameParameter):
+			oneParamSource = p
+
+	if(oneParamSource=={}):
+		return ""
+
+	value = ""
+	for out in oF['Outputs']:
+		if(out['StackName']==oneParamSource['stackName'] and \
+			out['OutputKey']==oneParamSource['outputKey']):
+			value = out['OutputValue']
+	return value
+
+
+def runStack(cfg, st, cfgOutFile):
+	stackFullName = cfg['stack-base-name'] + "-" + st['name']
+	fullFile = pathBuild(cfg, st['path'], st['template-file'])
+	outputDir = pathBuild(cfg, cfg['output-dir-stacks'], st['template-file'])
+
+	print("=======================================================================")
+	print("Stack Name: " +  stackFullName)
+	print("Stack Path: " +  st['path'])
+	print("Stack File: " +  st['template-file'])
+	
+	print()
+	print("$ package command .............")
+	bk = getOutputValue(st['parameters-source'], cfgOutFile, "bucketDeployPackages")
+	cmdPackage = " aws cloudformation package --template-file " + fullFile + \
+		" --output-template-file " + fullFile + "-deploy.yaml" + \
+		" --s3-bucket " + bk + " " \
+		" --region " + cfg['aws-region'] + \
+		" --profile " + cfg['profile-user']
+	runShell(cmdPackage, st['name'] + " package stack ...")
+
+	print()
+	print("$ deploy command .............")
+	cmdDeploy = "aws cloudformation deploy " + \
+    		" --region " + cfg['aws-region'] + \
+    		" --template-file " + fullFile + "-deploy.yaml " + \
+    		" --stack-name " + stackFullName + \
+    		" --capabilities CAPABILITY_IAM " + \
+    		" --profile " + cfg['profile-user'] + \
+    		" --tags Project=" + cfg['project-tag'] + \
+    		" --parameter-overrides TagProject=" + cfg['project-tag']
+	#runShellStack(cmdDeploy, cfg, stackFullName, st['name'] + " deploy stack ...", cfgOutFile)
 
 def main():
 	filePath = sys.argv[1]
 	cfg = cfgLoad(filePath)
 	awsSession(cfg)
-	configOutputFile = configOutputFileInitialize(cfg)
-	runPrerequisitesStack(cfg, configOutputFile)
-	#runStack(cfg, bucketDeployPackages)
+	cfgOutFile = configOutputFileInitialize(cfg)
+	runPrerequisitesStack(cfg, cfgOutFile)
+	
+	for st in cfg['stacks']:
+		runStack(cfg, st, cfgOutFile)
 
 if __name__ == '__main__':
 	main()
